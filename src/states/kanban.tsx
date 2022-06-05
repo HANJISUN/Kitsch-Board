b84bd90cdcecb3e2f632501300d7d7154432f203
jiiiooiiij <88502596+HANJISUN@users.noreply.github.com>
2022-06-05 12:32:06 +0900

    feat: 로컬스토리지 저장 기능 구현

--- src/states/kanban.tsx
+++ src/states/kanban.tsx
@@ -1,7 +1,25 @@
 import { atom } from 'recoil';
 import { Icardtype } from 'types/kanban';
 
+export const localStorageEffect =
+  (id: any) =>
+  ({ setSelf, onSet }: any) => {
+    const savedValue = localStorage.getItem(id);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue: any, _: any, isReset: boolean) => {
+      isReset ? localStorage.removeItem(id) : localStorage.setItem(id, JSON.stringify(newValue));
+    });
+  };
+
 export const kanbanListState = atom<Icardtype[]>({
   key: 'kanbanListState',
   default: [],
+  effects: [localStorageEffect(id)],
 });
+
+function id(id: any): import('recoil').AtomEffect<Icardtype[]> {
+  throw new Error('Function not implemented.');
+}
